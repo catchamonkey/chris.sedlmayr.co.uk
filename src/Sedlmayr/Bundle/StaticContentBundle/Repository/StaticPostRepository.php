64226b106bedea2f08699aa532424cddc8953a23
Chris Sedlmayr <chris@sedlmayr.co.uk>
2012-10-26 21:08:29 +0100

    [StaticContentBundle] Adds a Repository to fetch static posts
    * Works out a title and published date based on the title
    * allows setting of various directories in which to find posts

---
+++ src/Sedlmayr/Bundle/StaticContentBundle/Repository/StaticPostRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Sedlmayr\Bundle\StaticContentBundle\Repository;
+
+use Symfony\Component\Finder\Finder;
+
+/**
+ * This class handles data access for static posts
+ * @author Chris Sedlmayr <chris@sedlmayr.co.uk>
+ */
+class StaticPostRepository
+{
+    private $_finder;
+    private $_fileDirectories;
+
+    public function __construct(Finder $finder)
+    {
+        $this->_finder          = $finder;
+        $this->_fileDirectories = array();
+    }
+
+    public function addFileDirectory($fileDirectory)
+    {
+        $this->_fileDirectories[] = $fileDirectory;
+    }
+
+    public function getPosts()
+    {
+        $posts = array();
+        // look at each of the file directories and read the files
+        foreach ($this->_fileDirectories as $dir) {
+            if (is_dir(__DIR__.$dir)) {
+                $this->_finder->files()->in(__DIR__.$dir)->sortByName();
+
+                foreach ($this->_finder as $file) {
+
+                    $fileName           = $file->getFilename();
+                    $parts              = explode('-', $fileName);
+                    // drop the first three parts for later use (these were the date)
+                    $year               = array_shift($parts);
+                    $month              = array_shift($parts);
+                    $day                = array_shift($parts);
+                    // put back together with a space
+                    $file->title        = implode(' ', $parts);
+                    $file->published_at = new \DateTime($year.'-'.$month.'-'.$day);
+                    $posts[] = $file;
+                }
+            }
+        }
+
+        return $posts;
+    }
+}
